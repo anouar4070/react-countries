34078b9c76a15263a0d19116834a5e5d45f17aab
anouar garbaa <g.anouar@yahoo.com>
2024-05-28 12:36:58 +0100

    add loading button

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const searchInput = useRef("");
 
   const getAllCountries = async () => {
@@ -12,6 +14,8 @@ const Countries = () => {
       if (result.status === 200) setCountries(result.data);
     } catch (error) {
       console.error({ message: "error", error });
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -32,7 +36,6 @@ const Countries = () => {
     } catch (error) {
       console.error({ message: "error", error });
     }
-
   };
 
   const handleReset = () => {
@@ -40,14 +43,13 @@ const Countries = () => {
       searchInput.current.value = "";
       getAllCountries();
     }
-};
+  };
 
   useEffect(() => {
-    getAllCountries();
+    setTimeout(() => {
+      (async () => await getAllCountries())();
+    }, 2000);
   }, []);
-
- 
-  
 
   return (
     <>
@@ -56,44 +58,58 @@ const Countries = () => {
           <h1>List of Countries</h1>
         </div>
         <div className="col-md-4 text-right">
-
           <form className="d-flex" onSubmit={SearchCountries}>
-           
             <input
               ref={searchInput}
               className="form-control me-sm-2"
               type="search"
               placeholder="Search"
             />
-            
+
             <button className="btn btn-secondary my-2 my-sm-0" type="submit">
               Search
             </button>
-            
-            <button className="btn btn-danger my-2 my-sm-0" type="button"  onClick={handleReset}>
+
+            <button
+              className="btn btn-danger my-2 my-sm-0"
+              type="button"
+              onClick={handleReset}
+            >
               Reset
             </button>
-
           </form>
         </div>
       </div>
-      <div class="row">
-        {countries.map((country) => (
-          <div className="col-md-4">
-            <div className="card my-2">
-              <img
-                className="card-img-top"
-                src={country.flags.png}
-                alt="Title"
-              />
-              <div className="card-body">
-                <h4 className="card-title">{country.name.official} </h4>
-                <p className="card-text">Region: {country.region} </p>
-                <p className="card-text">Capital: {country.capital}</p>
+
+      <div className="row">
+        {isLoading && (
+          <button class="btn btn-primary" type="button" disabled>
+            <span
+              class="spinner-border spinner-border-sm"
+              role="status"
+              aria-hidden="true"
+            ></span>
+            Loading...
+          </button>
+        )}
+
+        {!isLoading &&
+          countries.map((country) => (
+            <div className="col-md-4">
+              <div className="card my-2">
+                <img
+                  className="card-img-top"
+                  src={country.flags.png}
+                  alt="Title"
+                />
+                <div className="card-body">
+                  <h4 className="card-title">{country.name.official} </h4>
+                  <p className="card-text">Region: {country.region} </p>
+                  <p className="card-text">Capital: {country.capital}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
